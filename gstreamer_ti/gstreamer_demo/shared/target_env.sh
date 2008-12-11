27e81cf46d826c52e5a77937340e2b23e84f7285
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2008-12-11 23:13:20 +0000

    Place the plugin registry cache in /tmp.
    This will allow GStreamer to run correctly on systems where the root file
    system is mounted read-only.  Most devices use a ramdisk for /tmp, so this
    should be safe.
    
    This issue was exposed while trying to run a GStreamer demo off of an SD car
    on OMAP 35xx, where the SD card contained the device file system and was
    mounted read-only.
    
    Some comments were also added to target_env.sh to explain the purpose of the
    environment variables being set.
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@40 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/shared/target_env.sh
+++ gstreamer_ti/gstreamer_demo/shared/target_env.sh
@@ -20,7 +20,19 @@ GSTREAMER_INSTALL_DIR=/opt/gstreamer
 GSTINSPECT=gst-inspect
 GSTLAUNCH=gst-launch
 
+# Make sure we store the registry cache in /tmp so we don't fail when running
+# from a read-only file system.  If we don't do this, GStreamer tries to
+# create the cache in the current directory.
+export GST_REGISTRY=/tmp/gst_registry.bin
+
+# Configure our LD_LIBRARY_PATH so GStreamer applications can find shared
+# object libraries while loading.
 export LD_LIBRARY_PATH=${GSTREAMER_INSTALL_DIR}/lib
+
+# Set the GST_PLUGIN_PATH to the directory where GStreamer should look for
+# its plugins
 export GST_PLUGIN_PATH=${GSTREAMER_INSTALL_DIR}/lib/gstreamer-0.10
+
+# Put the GStreamer binaries in our path
 export PATH=${GSTREAMER_INSTALL_DIR}/bin:$PATH
 
