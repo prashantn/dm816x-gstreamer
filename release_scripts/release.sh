f75d0bf660c4209581bfc83acdec3e1ba6a73e32
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2009-02-20 23:41:12 +0000

    Add script for creating release tarballs.
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@108 24075187-2e39-4e88-bbb8-bc8aa768f540

---
+++ release_scripts/release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+   echo 
+   echo "Usage:  $0 <release number>"
+   echo 
+   echo "Example:  $0 0.99.00"
+   echo 
+   exit 0
+fi
+
+REL=$1
+shift
+
+REL_US=`echo $REL | sed s#[.]#_#g`
+
+echo $REL
+echo $REL_US
+
+FULL_NAME="gst-ti-plugin-full-${REL}"
+MIN_NAME="gst-ti-plugin-minimal-${REL}"
+
+umask 0002
+svn export https://omapzoom.org/svn/gstreamer_ti/tags/TAG_RELEASE_${REL_US}/gstreamer_ti
+
+ln -s gstreamer_ti ${FULL_NAME}
+ln -s gstreamer_ti/ti_build/ticodecplugin ${MIN_NAME}
+tar chf - ${FULL_NAME} | gzip --best -c > ${FULL_NAME}.tar.gz
+tar chf - ${MIN_NAME}  | gzip --best -c > ${MIN_NAME}.tar.gz
+
