8cb5989d18ff6ebf23aa393139ad24c382b6e128
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2009-10-08 22:06:24 +0000

    Update loadmodules.sh for DM365.
    This fine-tunes DM365's loadmodules.sh to allocate buffers as needed for
    encode/decode scripts.
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@402 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
+++ gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
@@ -5,10 +5,9 @@ rmmod edmak 2>/dev/null
 rmmod dm365mmap 2>/dev/null
 
 # Pools configuration
-insmod cmemk.ko phys_start=0x85400000 phys_end=0x88000000 pools=1x2097152,7x829440,1x524288,1x108680,1x81920,2x8192,6x4096,8x96,64x56,1x2938880,1x6650880,4x675840,1x75840,8x1548288,1x5988352,1x6045696
+insmod cmemk.ko phys_start=0x85000000 phys_end=0x88000000 \
+        pools=1x6651904,1x3670016,6x1548288,1x282624,1x159744,3x118784,1x49152,1x32768,1x233472,1x28672,1x16384,3x12288,2x8192,34x4096
 
+insmod irqk.ko 
 insmod edmak.ko
-insmod irqk.ko
 insmod dm365mmap.ko
-lsmod
-
