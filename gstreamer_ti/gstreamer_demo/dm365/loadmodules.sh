11fe8de1aec50f25a6de8ed972e9b2b873effa78
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2009-10-14 23:59:18 +0000

    Refine loadmodules.sh on DM365 for H.264 playback.
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@418 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
+++ gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
@@ -5,8 +5,8 @@ rmmod edmak 2>/dev/null
 rmmod dm365mmap 2>/dev/null
 
 # Pools configuration
-insmod cmemk.ko phys_start=0x85000000 phys_end=0x88000000 \
-        pools=1x6651904,1x3670016,6x1548288,1x282624,1x159744,3x118784,1x49152,1x32768,1x233472,1x28672,1x16384,3x12288,2x8192,34x4096
+insmod cmemk.ko phys_start=0x85400000 phys_end=0x88000000 \
+    pools=1x6651904,1x3670016,18x1548288,1x282624,1x159744,1x49152,1x32768,1x28672,1x16384,3x12288,2x8192,36x4096
 
 insmod irqk.ko 
 insmod edmak.ko
