664769a71bca27d9c4cc20583b81f8bef2e5a097
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2010-07-02 20:43:39 +0000

    Create a "TIDmaiBufTab" buffer table object
    This object is used encapsulate DMAI BufTab objects inside of a GstMiniObjects.
    The allows us to postpone deleting a BufTab until all of its buffers have been
    unrefed, even if the element that created the BufTab no longer needs it.
    
    Attention DVSDK 2.00 Developers
    ===============================
    The commits immediately following this one will start changing existing
    elements to use GstTIDmaiBufTab objects instead of directly using DMAI's BufTab
    objects.  When these changes are made, they expose a bug in linuxutils 2.23.01.
    
    The bug is that CMEM will erroneously throw an error if a CMEM buffer is freed
    in a different thread than the one that allocated it.  CMEM is trying to ensure
    that the process freeing a buffer is the same one that created it, but it is
    getting confused because different threads have different process IDs and it
    thinks they are different processes.
    
    We have no control over what thread will free a GstTIDmaiBufTab object, as it
    ends up getting freed by the last thread that used it.  If you use
    multi-threaded pipelines (which is very common), this bug manifests as errors
    like the following when the pipeline shuts down:
    
    CMEMK Error: FREE: Not a registered user of physical buffer 0x87968000
    CMEM Error: free: failed to free 0x43316000
    
    To resolve this issue, you must update your linuxutils component to version
    2.24.04.05.  Linuxutils 2.24.04.05 can be downloaded from this page:
    
    http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/linuxutils/index.html
    
    Un-tar the updated version of linuxutils into your DVSDK 2.00 directory, and
    update the following line in your Rules.make:
    
    Old setting:
    CMEM_INSTALL_DIR=$(DVSDK_INSTALL_DIR)/linuxutils_2_23_01
    
    New setting:
    CMEM_INSTALL_DIR=$(DVSDK_INSTALL_DIR)/linuxutils_2_24_04_05
    
    Then re-build the DVSDK components as follows:
    
    1)  Change to the DVSDK directory
    2)  Double-check your Rules.make settings if needed to make sure all the
        components point to valid component directories
    3)  Run "make linux"
    4)  Run "make clean; make"
    
    This will re-build the DVSDK components with the newer version of linuxutils,
    and the next time you build and install the TI GStreamer plugin it should pick
    up the fix.
    
    Make sure the gstreamer_demo directory gets re-installed, otherwise you won't
    get the updated version of cmemk.ko.  Also make sure cmemk.ko gets re-loaded.
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@708 24075187-2e39-4e88-bbb8-bc8aa768f540

---
+++ gstreamer_ti/ti_build/ticodecplugin/src/gsttidmaibuftab.c
@@ -0,0 +1,209 @@
+/*
+ * gsttidmaibuftab.c
+ * 
+ * The "GstTIDmaiBufTab" object is used encapsulate DMAI BufTab objects inside
+ * of a GstMiniObjects.  The allows us to postpone deleting a BufTab until all
+ * of its buffers have been unrefed, even if the element that created the
+ * BufTab no longer needs it.
+ * 
+ * Original Author:
+ *     Don Darling, Texas Instruments, Inc.
+ *
+ * Copyright (C) 2008-2010 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation version 2.1 of the License.
+ *
+ * This program is distributed #as is# WITHOUT ANY WARRANTY of any kind,
+ * whether express or implied; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include <stdlib.h>
+
+#include <ti/sdo/dmai/Dmai.h>
+#include <ti/sdo/dmai/BufTab.h>
+#include <ti/sdo/dmai/Buffer.h>
+#include <ti/sdo/dmai/Rendezvous.h>
+
+#include "gsttidmaibuftab.h"
+
+/* Declare variable used to categorize GST_LOG output */
+GST_DEBUG_CATEGORY_STATIC (gst_tidmaibuftab_debug);
+#define GST_CAT_DEFAULT gst_tidmaibuftab_debug
+
+/* Declare a global pointer to our buffer base class */
+static GstMiniObjectClass *parent_class;
+
+/* Static Function Declarations */
+static void
+    gst_tidmaibuftab_init(GstTIDmaiBufTab *self);
+static void
+    gst_tidmaibuftab_log_init(void);
+static void
+    gst_tidmaibuftab_class_init(GstTIDmaiBufTabClass *klass);
+static void
+    gst_tidmaibuftab_finalize(GstTIDmaiBufTab *self);
+
+/* Define GST_TYPE_TIDMAIBUFTAB */
+G_DEFINE_TYPE_WITH_CODE (GstTIDmaiBufTab, gst_tidmaibuftab, \
+    GST_TYPE_MINI_OBJECT, gst_tidmaibuftab_log_init());
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_log_init
+ *    Initialize the GST_LOG for this type
+ ******************************************************************************/
+static void gst_tidmaibuftab_log_init(void)
+{
+    GST_DEBUG_CATEGORY_INIT(gst_tidmaibuftab_debug,
+        "GstTIDmaiBufTab", 0, "TI DMAI Buffer Table (BufTab)");
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_init
+ *    Initializes a new transport buffer instance.
+ ******************************************************************************/
+static void gst_tidmaibuftab_init(GstTIDmaiBufTab *self)
+{
+    GST_LOG("begin init\n");
+
+    self->hBufTab = NULL;
+    self->hRv     = NULL;
+
+    GST_LOG("end init\n");
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_class_init
+ *    Initializes the GstTIDmaiBufTab class.
+ ******************************************************************************/
+static void gst_tidmaibuftab_class_init(
+                GstTIDmaiBufTabClass *klass)
+{
+    GstMiniObjectClass *mo_class = GST_MINI_OBJECT_CLASS(klass);
+
+    GST_LOG("begin class_init\n");
+
+    parent_class = g_type_class_peek_parent(klass);
+
+    /* Override the mini-object's finalize routine so we can do cleanup when
+     * a GstTIDmaiBufTab is unref'd.
+     */
+    mo_class->finalize =
+        (GstMiniObjectFinalizeFunction) gst_tidmaibuftab_finalize;
+
+    GST_LOG("end class_init\n");
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_finalize
+ *    Dispose a DMAI BufTab object.
+ ******************************************************************************/
+static void gst_tidmaibuftab_finalize(GstTIDmaiBufTab *self)
+{
+    GST_LOG("begin finalize\n");
+
+    if (self->hBufTab) {
+        BufTab_delete(self->hBufTab);
+        self->hBufTab = NULL;
+    }
+
+    if (self->hRv) {
+        Rendezvous_delete(self->hRv);
+        self->hRv = NULL;
+    }
+
+    /* Call GstMiniObject's finalize routine, so our base class can do its
+     * cleanup as well.  If we don't do this, we could end up with a memory
+     * leak that is very difficult to track down.
+     */
+    GST_MINI_OBJECT_CLASS(parent_class)->finalize(GST_MINI_OBJECT(self));
+
+    GST_LOG("end finalize\n");
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_get_buf
+ *    Return a free buffer from the DMAI BufTab object.
+ ******************************************************************************/
+Buffer_Handle gst_tidmaibuftab_get_buf(GstTIDmaiBufTab *self)
+{
+    Buffer_Handle hFreeBuf = NULL;
+
+    /* Get a free buffer from the BufTab */
+    hFreeBuf = BufTab_getFreeBuf(self->hBufTab);
+    while (!hFreeBuf) {
+        Rendezvous_meet(self->hRv);
+        hFreeBuf = BufTab_getFreeBuf(self->hBufTab);
+        Rendezvous_reset(self->hRv);
+    }
+
+    return hFreeBuf;
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_new
+ *    Create a new DMAI BufTab object.
+ ******************************************************************************/
+GstTIDmaiBufTab* gst_tidmaibuftab_new(gint num_bufs, gint32 size,
+                     Buffer_Attrs *attrs)
+{
+    Rendezvous_Attrs  rzvAttrs = Rendezvous_Attrs_DEFAULT;
+    GstTIDmaiBufTab  *self;
+
+    GST_LOG("begin new\n");
+
+    self = (GstTIDmaiBufTab*)gst_mini_object_new(GST_TYPE_TIDMAIBUFTAB);
+    g_return_val_if_fail(self != NULL, NULL);
+
+    self->hBufTab = BufTab_create(num_bufs, size, attrs);
+    self->hRv     = Rendezvous_create(Rendezvous_INFINITE, &rzvAttrs);
+
+    if (!self->hBufTab || !self->hRv) {
+        GST_ERROR("Failed to create a new GstTIDmaiBufTab object");
+        gst_mini_object_unref(GST_MINI_OBJECT(self));
+        return NULL;
+    }
+
+    return self;
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_ref
+ *    Add a reference to a DMAI BufTab object.
+ ******************************************************************************/
+void gst_tidmaibuftab_ref(GstTIDmaiBufTab *self)
+{
+    gst_mini_object_ref(GST_MINI_OBJECT(self));
+}
+
+
+/******************************************************************************
+ * gst_tidmaibuftab_unref
+ *    Un-ref a DMAI BufTab object.
+ ******************************************************************************/
+void gst_tidmaibuftab_unref(GstTIDmaiBufTab *self)
+{
+    gst_mini_object_unref(GST_MINI_OBJECT(self));
+}
+
+
+/******************************************************************************
+ * Custom ViM Settings for editing this file
+ ******************************************************************************/
+#if 0
+ Tabs (use 4 spaces for indentation)
+ vim:set tabstop=4:      /* Use 4 spaces for tabs          */
+ vim:set shiftwidth=4:   /* Use 4 spaces for >> operations */
+ vim:set expandtab:      /* Expand tabs into white spaces  */
+#endif
---
+++ gstreamer_ti/ti_build/ticodecplugin/src/gsttidmaibuftab.h
@@ -0,0 +1,95 @@
+/*
+ * gsttidmaibuftab.h
+ * 
+ * The "GstTIDmaiBufTab" object is used encapsulate DMAI BufTab objects inside
+ * of a GstMiniObjects.  The allows us to postpone deleting a BufTab until all
+ * of its buffers have been unrefed, even if the element that created the
+ * BufTab no longer needs it.
+ * 
+ * Original Author:
+ *     Don Darling, Texas Instruments, Inc.
+ *
+ * Copyright (C) 2010 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation version 2.1 of the License.
+ *
+ * This program is distributed #as is# WITHOUT ANY WARRANTY of any kind,
+ * whether express or implied; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef __GST_TIDMAIBUFTAB_H__
+#define __GST_TIDMAIBUFTAB_H__
+
+#include <gst/gst.h>
+
+#include <ti/sdo/dmai/Dmai.h>
+#include <ti/sdo/dmai/BufTab.h>
+#include <ti/sdo/dmai/Buffer.h>
+#include <ti/sdo/dmai/Rendezvous.h>
+
+G_BEGIN_DECLS
+
+/* Type macros for GST_TYPE_TIDMAIBUFTAB */
+#define GST_TYPE_TIDMAIBUFTAB \
+    (gst_tidmaibuftab_get_type())
+#define GST_TIDMAIBUFTAB(obj) \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_TIDMAIBUFTAB, \
+    GstTIDmaiBufTab))
+#define GST_IS_TIDMAIBUFTAB(obj) \
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_TIDMAIBUFTAB))
+#define GST_TIDMAIBUFTAB_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_TIDMAIBUFTAB, \
+    GstTIDmaiBufTabClass))
+#define GST_IS_TIDMAIBUFTAB_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_TIDMAIBUFTAB))
+#define GST_TIDMAIBUFTAB_GET_CLASS(obj) \
+    (G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_TIDMAIBUFTAB, \
+    GstTIDmaiBufTabClass))
+
+/* Utility macros */
+#define GST_TIDMAIBUFTAB_BUFTAB(obj) \
+    ((obj) ? GST_TIDMAIBUFTAB(obj)->hBufTab : NULL)
+#define GST_TIDMAIBUFTAB_MUTEX(obj) \
+    ((obj) ? GST_TIDMAIBUFTAB(obj)->hRv : NULL)
+
+typedef struct _GstTIDmaiBufTab      GstTIDmaiBufTab;
+typedef struct _GstTIDmaiBufTabClass GstTIDmaiBufTabClass;
+
+/* _GstTIDmaiBufTab object */
+struct _GstTIDmaiBufTab {
+    GstMiniObject     parent_instance;
+    BufTab_Handle     hBufTab;
+    Rendezvous_Handle hRv;
+};
+
+struct _GstTIDmaiBufTabClass {
+    GstMiniObjectClass    derived_methods;
+};
+
+/* External function declarations */
+GType            gst_tidmaibuftab_get_type(void);
+GstTIDmaiBufTab* gst_tidmaibuftab_new(gint num_bufs, gint32 size,
+                     Buffer_Attrs *attrs);
+Buffer_Handle    gst_tidmaibuftab_get_buf(GstTIDmaiBufTab *self);
+void             gst_tidmaibuftab_ref(GstTIDmaiBufTab *self);
+void             gst_tidmaibuftab_unref(GstTIDmaiBufTab *self);
+
+G_END_DECLS 
+
+#endif /* __GST_TIDMAIBUFTAB_H__ */
+
+
+/******************************************************************************
+ * Custom ViM Settings for editing this file
+ ******************************************************************************/
+#if 0
+ Tabs (use 4 spaces for indentation)
+ vim:set tabstop=4:      /* Use 4 spaces for tabs          */
+ vim:set shiftwidth=4:   /* Use 4 spaces for >> operations */
+ vim:set expandtab:      /* Expand tabs into white spaces  */
+#endif
