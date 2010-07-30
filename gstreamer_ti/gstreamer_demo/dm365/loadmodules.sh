32fd69625f2dae7e11158fbed665c920c1d8cc39
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2010-07-30 15:46:03 +0000

    Create a loadmodules.sh for DM365 on DVSDK 3.10.
    CMEM allocation creates enough contiguous buffers for the worst case
    requirements of typical GStreamer example use-cases.  This configuration
    should handle:
    
    * 720P video decode
    * 720P Capture + Resize + Encode to .mp4 container
    * UYVY/NV12 Video loopback (accelerated and unaccelerated)
    * MP4 (H.264 + AAC) decode.
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@767 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
+++ gstreamer_ti/gstreamer_demo/dm365/loadmodules.sh
@@ -5,9 +5,11 @@ rmmod edmak 2>/dev/null
 rmmod dm365mmap 2>/dev/null
 
 # Pools configuration
-insmod cmemk.ko phys_start=0x85400000 phys_end=0x88000000 \
-    pools=1x6651904,1x3670016,18x1548288,1x282624,1x159744,1x49152,1x32768,1x28672,1x16384,3x12288,2x8192,36x4096
+insmod cmemk.ko phys_start=0x84d00000 phys_end=0x88000000 pools=1x16539648,1x4841472,4x1843200,14x1646592,1x282624,1x176128,1x147456,1x69632,1x61440,1x32768,2x20480,1x16384,1x12288,4x8192,69x4096
 
 insmod irqk.ko 
 insmod edmak.ko
 insmod dm365mmap.ko
+
+rm -f /dev/dm365mmap
+mknod /dev/dm365mmap c `awk "\\$2==\"dm365mmap\" {print \\$1}" /proc/devices` 0
