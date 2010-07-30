c18c85ff7fdf455c3e8eafadfedabd1ab78dbf72
don.darling <don.darling@24075187-2e39-4e88-bbb8-bc8aa768f540>
2010-07-30 15:46:29 +0000

    Create a loadmodules.sh for DM355 on DVSDK 3.10.
    CMEM allocation creates enough contiguous buffers for the worst case
    requirements of typical GStreamer example use-cases.  This configuration
    should handle:
    
    * NTSC/PAL MPEG-4 Video Decode
    * NTSC/PAL MPEG-4 Capture + Resize + Encode
    * JPEG Image Encode
    * JPEG Image Decode
    * UYVY Loopback (accelerated and unaccelerated)
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@770 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/dm355/loadmodules.sh
+++ gstreamer_ti/gstreamer_demo/dm355/loadmodules.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
-# CMEM Allocation
-#    1x3628800 Circular buffer
-#    4x829440  Video buffers (max D1 PAL)
-#    1x829440  Underlying software components (codecs, etc.)
-#    1x518400  Underlying software components (codecs, etc.)
-#    1x4948    Underlying software components (codecs, etc.)
-#    1x1505280 Underlying software components (codecs, etc.)
-#    1x5760    Underlying software components (codecs, etc.)
-#    1x8192    Underlying software components (codecs, etc.)
-#    1x1       Dummy buffer used during final flush
-insmod cmemk.ko phys_start=0x87400000 phys_end=0x88000000 \
-    pools=1x3628800,5x829440,1x518400,1x4948,1x1505280,1x5760,1x8192,1x1
+# Sample bootargs is given below
+# mem=93M console=ttyS0,115200n8 root=/dev/nfs rw nfsroot=<nfsroot> ip=dhcp video=davincifb:vid0=OFF:vid1=OFF:osd0=720x576x16,2025K
+
+rmmod cmemk 2>/dev/null
+rmmod irqk 2>/dev/null
+rmmod edmak 2>/dev/null
+rmmod dm350mmap 2>/dev/null
+
+# Handle any of the use-cases below
+insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=1x3629056,1x1531904,7x831488,2x16384,1x8192,73x4096
+
+# Image encode
+#insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=1x1658880,1x831488,1x45056,48x4096
+
+# Image decode
+#insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=1x3629056,1x692224,1x12288,55x4096
+
+# Resize Loopback
+#insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=3x831488
+
+# MPEG-4 Encode
+#insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=1x1658880,1x1531904,3x622592,1x81920,2x16384,1x8192,68x4096
+
+# MPEG-4 Decode
+#insmod cmemk.ko phys_start=0x87500000 phys_end=0x88000000 pools=1x2904064,1x1507328,4x831488,1x12288,2x8192,73x4096
 
 ./mapdmaq
 
+insmod irqk.ko 
+insmod edmak.ko
 insmod dm350mmap.ko
+
 rm -f /dev/dm350mmap
 mknod /dev/dm350mmap c `awk "\\$2==\"dm350mmap\" {print \\$1}" /proc/devices` 0
