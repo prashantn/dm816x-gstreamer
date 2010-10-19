5f2052f40e9fcb8836b8021ec6e3bbdaebd9ac91
brijesh.singh <brijesh.singh@24075187-2e39-4e88-bbb8-bc8aa768f540>
2010-10-19 18:36:15 +0000

    Add OMAP3530/DM3730 DVSDK 4.0 GA support
    * Patch drops DVSDK 3.x support for OMAP3.
    * Adding DVSDK 4.0 support is done by just renaming omap3530_dv400 with omap3530.
    * Since the DVSDK 4.0 GA codec server comes with c6accel codec integrated, enabling c6accel based elements on omap3530 platform.
    
    git-svn-id: https://gstreamer.ti.com/svn/gstreamer_ti/trunk@838 24075187-2e39-4e88-bbb8-bc8aa768f540

--- gstreamer_ti/gstreamer_demo/omap3530/loadmodules.sh
+++ gstreamer_ti/gstreamer_demo/omap3530/loadmodules.sh
@@ -7,25 +7,28 @@
 # 0x86300000    16 MB   CMEM
 # 0x87300000    13 MB   CODEC SERVER
 
+# remove previously inserted cmemk to ensure that we are using the right pool configuration.
+rmmod cmemk
+
 #
 # CMEM Allocation
 #    1x5250000 Circular buffer
 #    6x829440  Video buffers (max D1 PAL)
 #    1x345600  Underlying software components (codecs, etc.)
 #    1x1       Dummy buffer used during final flush
-insmod cmemk.ko phys_start=0x86300000 phys_end=0x87300000 \
+modprobe cmemk allowOverlap=1 phys_start=0x86300000 phys_end=0x87300000 \
     pools=1x5250000,6x829440,1x345600,1x1
 
+# insert Local Power Manager driver
+modprobe lpm_omap3530
+
 # insert DSP/BIOS Link driver
-insmod dsplinkk.ko
+modprobe dsplinkk
 
 # make /dev/dsplink
 rm -f /dev/dsplink
 mknod /dev/dsplink c `awk "\\$2==\"dsplink\" {print \\$1}" /proc/devices` 0
 
-# insert Local Power Manager driver
-insmod lpm_omap3530.ko
-
 # insert SDMA driver
-insmod sdmak.ko
+modprobe sdmak
 
