c8be229e5ed9eaf70ed0f4e2156c00440c288857
Brijesh Singh <bksingh@ti.com>
2011-06-15 17:17:32 -0500

    set sync=false for elem decoding

--- gstreamer_ti/gstreamer_demo/dm816x/example_pipeline.sh
+++ gstreamer_ti/gstreamer_demo/dm816x/example_pipeline.sh
@@ -10,7 +10,7 @@ export PATH=/opt/gstreamer/bin:$PATH
 echo 0 > /sys/devices/platform/vpss/graphics0/enabled
 
 # pipeline decode elemenatry H.264 stream
-gst-launch filesrc location=sample.264  ! typefind ! h264parse access-unit=true ! omx_h264dec ! omx_colorconv ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60 ! gstperf ! omx_videosink -v
+gst-launch filesrc location=sample.264  ! typefind ! h264parse access-unit=true ! omx_h264dec ! omx_colorconv ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60 ! gstperf ! omx_videosink sync=false -v
 
 # pipeline to decode MP4 container (H.264 + AAC). Note that currently AV is disabled.
 gst-launch -v filesrc location=sample.mp4 ! qtdemux name=demux demux.audio_00 ! queue max-size-buffers=8000 max-size-time=0 max-size-bytes=0 ! faad ! alsasink sync=false demux.video_00 ! queue !  nal2bytestream_h264  !  omx_h264dec ! omx_colorconv ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60  ! gstperf ! omx_videosink sync=false
