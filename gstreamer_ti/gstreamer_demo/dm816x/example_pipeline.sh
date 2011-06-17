7f05f75e9966a496d6e0c47a5ec9e17c0e0c8dfa
Brijesh Singh <bksingh@ti.com>
2011-06-17 11:41:08 -0500

    add more example pipeline

--- gstreamer_ti/gstreamer_demo/dm816x/example_pipeline.sh
+++ gstreamer_ti/gstreamer_demo/dm816x/example_pipeline.sh
@@ -13,3 +13,10 @@ gst-launch filesrc location=sample.264  ! typefind ! h264parse access-unit=true 
 # pipeline to decode MP4 container (H.264 + AAC). Note that currently AV is disabled.
 gst-launch -v filesrc location=sample.mp4 ! qtdemux name=demux demux.audio_00 ! queue max-size-buffers=8000 max-size-time=0 max-size-bytes=0 ! faad ! alsasink sync=false demux.video_00 ! queue !  nal2bytestream_h264  !  omx_h264dec ! omx_colorconv ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60  ! gstperf ! omx_videosink sync=false
 
+# pipeline to display the videotest pattern using omx sink
+gst-launch -v videotestsrc ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60  ! omx_videosink sync=false 
+
+# pipeline to color convert videotestsrc pattern from NV12 to YUY2 using HW accelerated omx element and then display using omx sink
+gst-launch -v videotestsrc ! omx_colorconv ! omx_ctrl display-mode=OMX_DC_MODE_1080P_60  ! omx_videosink sync=false
+
+
