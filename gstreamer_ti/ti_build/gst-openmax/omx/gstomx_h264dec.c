b96a41c9f1ab27b873107b0c4a825b5effa34667
Prashant Nandakumar <prashant.n@ti.com>
2011-10-04 08:36:18 +0530

    Fix bug in h264parse, V4l2sink userptr, set framerate of OMX h264 decoder to 60 	modified:   gstreamer_ti/opensource_build/Packages.make.dm816x 	new file:   gstreamer_ti/opensource_build/patchfiles/gst-plugins-bad-0.10.21/0007-h264parse-fix-boundary-condition.patch 	modified:   gstreamer_ti/opensource_build/patchfiles/gst-plugins-good-0.10.27/0012-v4l2sink-Add-Userptr-support.patch 	modified:   gstreamer_ti/ti_build/gst-openmax/omx/gstomx_h264dec.c

--- gstreamer_ti/ti_build/gst-openmax/omx/gstomx_h264dec.c
+++ gstreamer_ti/ti_build/gst-openmax/omx/gstomx_h264dec.c
@@ -99,11 +99,12 @@ initialize_port (GstOmxBaseFilter *omx_base)
     height = self->extendedParams.height;
 
     paramPort.nPortIndex = 1;
-	paramPort.nBufferCountActual = 6;//output_buffer_count
+	paramPort.nBufferCountActual = 20;//15;//output_buffer_count
     paramPort.format.video.nFrameWidth = width;
     paramPort.format.video.nFrameHeight = height;
     paramPort.format.video.eCompressionFormat = OMX_VIDEO_CodingUnused;
     paramPort.format.video.eColorFormat = OMX_COLOR_FormatYUV420SemiPlanar;
+	paramPort.format.video.xFramerate = (60) << 16;
 
     GST_DEBUG_OBJECT (self, "nFrameWidth = %ld, nFrameHeight = %ld, nBufferCountActual = %ld",
       paramPort.format.video.nFrameWidth, paramPort.format.video.nFrameHeight, 
@@ -112,6 +113,11 @@ initialize_port (GstOmxBaseFilter *omx_base)
     GST_DEBUG_OBJECT (self, "G_OMX_PORT_SET_DEFINITION (output)");
     G_OMX_PORT_SET_DEFINITION (omx_base->out_port, &paramPort);
 
+	G_OMX_PORT_GET_DEFINITION (omx_base->in_port, &paramPort);
+    //paramPort.nBufferCountActual = 8;
+    paramPort.format.video.xFramerate = (60) << 16;
+    G_OMX_PORT_SET_DEFINITION (omx_base->in_port, &paramPort);
+#if 0
     port = g_omx_core_get_port (gomx, "input", 0);
 
     GST_DEBUG_OBJECT(self, "SendCommand(PortEnable, %d)", port->port_index);
@@ -125,7 +131,7 @@ initialize_port (GstOmxBaseFilter *omx_base)
     OMX_SendCommand (g_omx_core_get_handle (port->core),
             OMX_CommandPortEnable, port->port_index, NULL);
     g_sem_down (port->core->port_sem);
-
+#endif
     GST_INFO_OBJECT (omx_base, "end");
 }
 
